705784fc8ce5790f6d084805920f688de30411b7
posttsoi <deserza@gmail.com>
2020-03-28 23:56:43 +0700

    add write_csv_from_json function

--- search_groups.py
+++ search_groups.py
@@ -1,10 +1,29 @@
 from access_token import TOKEN
 import requests
+import json
+
+
+def write_csv_from_json(response: json) -> None:
+    """"""
+    list_group = response['response']['items']
 
 
 def main():
-    r = requests.get(f'https://api.vk.com/method/groups.search', params={'q': 'qwd', 'access_token': TOKEN, 'v': 5.103})
-    print(r.json())
+    key_word = 'a'
+
+    params = {
+        'q': key_word,
+        'access_token': TOKEN,
+        'v': 5.103,
+        'type': 'group',
+        'sort': 3,
+        "count": 1000,
+        'offset': 0
+    }
+
+    re = requests.get(f'https://api.vk.com/method/groups.search', params)
+
+    write_csv_from_json(re.json())
 
 
 if __name__ == "__main__":
